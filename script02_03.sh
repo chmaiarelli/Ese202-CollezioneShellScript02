1ed99b61e734109992a6b2e0d5a36d45b42df1f0
chmaiarelli <christianmaiarelli@gmail.com>
2015-03-10 16:46:13 +0100

    Update script02_03.sh

--- script02_03.sh
+++ script02_03.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
-
-#
-# 1) Nota il risultato del comando cat accounts.txt
-#
-# Questo crea, nella directory corrente, una directory per ogni account riportato nel file accounts.txt
+@@ -1,6 +1,11 @@
+ #!/bin/bash
++nomiDirectory=`cat accounts.txt`
++for i in $nomiDirectory
++do
++	`mkdir $i`
++done
+ 
+ #
+ # 1) Nota il risultato del comando cat accounts.txt
+ #
+-# Questo crea, nella directory corrente, una directory per ogni account riportato nel file accounts.txt
++# Questo crea, nella directory corrente, una directory per ogni account riportato nel file accounts.txt
