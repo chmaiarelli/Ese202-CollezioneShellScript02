7312d38a8e02bb71dcdef26272f5b5d19a33c10c
chmaiarelli <christianmaiarelli@gmail.com>
2015-03-10 16:47:14 +0100

    Update script02_04.sh

--- script02_04.sh
+++ script02_04.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-
-#
-# Questo script cancella tutte le sottodirectory (e il loro eventuale contentuo) della directory corrente,
-# il cui nome è contenuto nel file accounts.txt
+@@ -1,5 +1,10 @@
+ #!/bin/bash
++directory=`cat accounts.txt`
++for i in $directory
++do
++	`rm -r $i`
++done
+ 
+ #
+ # Questo script cancella tutte le sottodirectory (e il loro eventuale contentuo) della directory corrente,
+-# il cui nome è contenuto nel file accounts.txt
++# il cui nome è contenuto nel file accounts.txt
