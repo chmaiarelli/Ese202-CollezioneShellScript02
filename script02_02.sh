55e2486051bd5d8b471335d6b41d52d4ba013d91
chmaiarelli <christianmaiarelli@gmail.com>
2015-03-10 16:45:11 +0100

    Update script02_02.sh

--- script02_02.sh
+++ script02_02.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
-
-#
-# Questo script riceve un argomento. Esso contiene un _prefisso_ per il nome di una directory.
-# Lo script crea 10 directory, nella directory corrente, con il prefisso fornito e un numero.
-# Esempio: prefisso = toscano -> directory: toscano1, toscano2, toscano3, ... toscano10
+ #!/bin/bash
++if [ $# -gt 1 -o $# -eq 0 ]
++then
++	echo
++	echo "Errore! Inserire uno e un solo argomento!"
++	exit 1
++fi
++for i in $(seq 10)
++do
++	mkdir $@$i
++done
++
++exit 0
+ 
+ #
+ # Questo script riceve un argomento. Esso contiene un _prefisso_ per il nome di una directory.
+ # Lo script crea 10 directory, nella directory corrente, con il prefisso fornito e un numero.
+ # Esempio: prefisso = toscano -> directory: toscano1, toscano2, toscano3, ... toscano10
